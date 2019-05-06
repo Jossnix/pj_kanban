a1e15fa7cc7660d4a0d2bb16c1de02c776a03695
jossnix <jossnix@mail.ru>
2019-05-06 22:32:51 +0300

    пользователь создаётся только если такого name не существует. Подключение к базе и завершение сеанса внесено в функцию, т.к. в противном случае возникают проблемы с callback

--- server/src/dbFunctions.js
+++ server/src/dbFunctions.js
@@ -4,24 +4,38 @@ const dbconf = require("./conf/dbconf.json");
 const url = dbconf.kUrl;
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
  
-mongoClient.connect(async function(err, client){
-    const db = client.db(dbconf.kName);
-    const collectionUs = db.collection(dbconf.kCollections.users);
-    await createUser(collectionUs, "Ivan", "Иван Иванович", "222", "user");
-    let res = await checkUser(collectionUs, "Andrew");
-    await delUser(collectionUs, "Andrew");
-    console.log("res:", res);
-    client.close();
-});
+createUser(dbconf, "Dima4", "Иван Иванович", "222", "user");
+    // let res = await checkUser(collectionUs, "Andrew");
+    // await delUser(collectionUs, "Andrew");
+    // console.log("res:", res);
+
 // Добавить пользователя
-async function createUser (collection, newUser, userDiscr, password, userRole) {
-  let user = {name: newUser, nameDiscr: userDiscr, pas: password, role: userRole}
-  await collection.insertOne(user, function(err, result){
-    if(err){ 
-        return console.log(err);
-    }
-    console.log(result.ops);
-  });
+async function createUser (dbconf, newUser, userDiscr, password, userRole) {
+  mongoClient.connect(async function(err, client){
+      const db = client.db(dbconf.kName);
+      const collectionUs = db.collection(dbconf.kCollections.users);
+      let user = {name: newUser, nameDiscr: userDiscr, pas: password, role: userRole}
+      await collectionUs.find({name: newUser}).toArray(async function(err, results){
+        if(err){ 
+          return console.log(err);
+        } else {
+          if(results.length === 0) {
+            console.log("Done");
+            await collectionUs.insertOne(user, function(err, result){
+              if(err){ 
+                  return console.log(err);
+              } else {
+                console.log(result.ops);
+                client.close();
+              }
+            })
+          } else {
+            console.log("User: " + newUser + " exist.");
+            client.close();
+          }
+        }
+      })
+  })
 }
 // Проверить, существует ли пользователь
 async function checkUser (collection, oldUser) {
