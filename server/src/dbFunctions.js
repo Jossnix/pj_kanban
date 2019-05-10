522f4cfe71f010ff4bb58ed94deffaac13bf938c
jossnix <jossnix@mail.ru>
2019-05-10 19:37:03 +0300

    исправлен поиск пользователя и его удаление

--- server/src/dbFunctions.js
+++ server/src/dbFunctions.js
@@ -4,10 +4,9 @@ const dbconf = require("./conf/dbconf.json");
 const url = dbconf.kUrl;
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
  
-createUser(dbconf, "Dima4", "Иван Иванович", "222", "user");
-    // let res = await checkUser(collectionUs, "Andrew");
-    // await delUser(collectionUs, "Andrew");
-    // console.log("res:", res);
+// createUser(dbconf, "Dima4", "Иван Иванович", "222", "user");
+delUser(dbconf, "Dima4");
+// checkUser(dbconf, "Dima");
 
 // Добавить пользователя
 async function createUser (dbconf, newUser, userDiscr, password, userRole) {
@@ -38,22 +37,60 @@ async function createUser (dbconf, newUser, userDiscr, password, userRole) {
   })
 }
 // Проверить, существует ли пользователь
-async function checkUser (collection, oldUser) {
-  await collection.find({name: oldUser}).toArray(function(err, results){
-    if(err){ 
-      return console.log(err);
-    }
-    console.log(results);
-    return (results)
+function checkUser (dbconf, oldUser) {
+  mongoClient.connect(async function(err, client){
+    const db = client.db(dbconf.kName);
+    const collectionUs = db.collection(dbconf.kCollections.users)
+    await collectionUs.find({name: oldUser}).toArray(function(err, results){
+      if(err){ 
+        return console.log(err);
+      } else {
+        console.log("Finds: ", results);
+        client.close();
+        return (results)
+      }
+    });
   });
 }
 // Удалить пользователя
-async function delUser (collection, oldUser) {
-  await collection.deleteOne({name: oldUser}, function(err, results){
-    if(err){ 
-      return console.log(err);
-    }
-    console.log("del: ", oldUser);
+async function delUser (dbconf, oldUser) {
+//   mongoClient.connect(async function(err, client){
+//     const db = client.db(dbconf.kName);
+//     const collectionUs = db.collection(dbconf.kCollections.users);
+//     await collectionUs.deleteOne({name: oldUser}, function(err, results){
+//       if(err){ 
+//         return console.log(err);
+//       }
+//       console.log("del: ", oldUser);
+//       client.close();
+//     });
+//   })
+// }
+
+
+  mongoClient.connect(async function(err, client){
+    const db = client.db(dbconf.kName);
+    const collectionUs = db.collection(dbconf.kCollections.users)
+    await collectionUs.find({name: oldUser}).toArray(async function(err, results){
+      if(err){ 
+        return console.log(err);
+      } else {
+        if(results.length !== 0) {
+          console.log(oldUser + " exist.");
+          await collectionUs.deleteOne({name: oldUser}, function(err, results){
+            if(err){ 
+                return console.log(err);
+            } else {
+              console.log("del: ", oldUser);
+              client.close();
+            }
+          })
+        } else {
+          console.log("User: " + oldUser + " not exist.");
+          client.close();
+        };
+      }
+    });
   });
 }
 // Изменить пароль/роль выбранному пользователю
