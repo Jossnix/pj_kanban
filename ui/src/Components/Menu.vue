9de1108c440bfe13b8d2f9745ace8ffd8f379b12
jossnix <jossnix@mail.ru>
2019-06-12 23:23:40 +0300

    текущий вариант меню со старыми комментариями

--- ui/src/Components/Menu.vue
+++ ui/src/Components/Menu.vue
@@ -2,12 +2,29 @@
     <div>
       <div id = "menuPanel">
         <div id = "blockMenu" v-show = "isLogin">
+          <b-button-group>
           <!-- <input type="text" value="Моя страница" readonly @click="goPage('/')">
           <input type="text" value="Проект" readonly @click="goPage('/pj')">
           <input type="text" value="Управление" readonly @click="goPage('/manager')"> -->
-          <span @click="goPage('/')" class="itemMenu">Моя страница</span>
+          <!-- <span @click="goPage('/')" class="itemMenu">Фильтры</span> -->
+          <b-nav-item @click="findDropMenu(item)">Фильтры</b-nav-item>
+          <!-- <span @click="goPage('/')" class="itemMenu">Задачи</span> -->
+          <!-- <b-dropdown id="dropdown-1" text="Задачи" class="m-md-2"> -->
+          <b-nav-item-dropdown id="dropdown-1" text="Задачи">
+            <b-dropdown-item>Создать задачу</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item-dropdown id="dropdown-2" text="Пользователи" class="m-md-2">
+            <b-dropdown-item>Создать учётную запись</b-dropdown-item>
+            <b-dropdown-item>Изменить учётную запись</b-dropdown-item>
+            <b-dropdown-item>Удалить учётную запись</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item @click="findDropMenu(item)">Изменить мои данные</b-nav-item>
+          <!-- <span @click="goPage('/')" class="itemMenu">Изменить мои данные</span> -->
+          </b-button-group>
+          <!-- <span @click="goPage('/')" class="itemMenu">Моя страница</span>
           <span @click="goPage('/pj')" class="itemMenu">Проект</span>
-          <span @click="goPage('/manager')" class="itemMenu">Управление</span>
+          <span @click="goPage('/manager')" class="itemMenu">Управление</span> -->
+                  <!-- фильтры задачи пользователи изменить мои данные -->
         </div>
         <div id = "blockExit" v-show = "isLogin">
           <button id = "btnExit" @click="pressBtnExit()">Выход</button>
@@ -17,8 +34,11 @@
 </template>
 
 <script>
-// import Vue from 'vue'
+import Vue from 'vue'
 import store from '../../Store/index.js'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
 
 export default {
   data () {
@@ -55,7 +75,7 @@ export default {
 #menuPanel {
     display: grid;
     grid-template-rows: 1fr;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1.5fr 0.5fr;
     align-items: center;
     width: 100%;
     color: rgb(231, 231, 231);
